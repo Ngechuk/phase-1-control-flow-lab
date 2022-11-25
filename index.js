ac418cd42057ebe97723400ce08db83609d8a0db
Kevin <kelvinngechu@gmail.com>
2022-11-25 19:24:27 +0300

    function ride

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 2000 && ride < 2501) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+} // Write your code here!
+
 
 function ternaryCheckCity(){
   // Write your code here!
